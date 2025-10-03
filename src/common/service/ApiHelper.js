b89d69c26dad23e219fb21c381443b8280408760
Ansh Talaviya <159028042+AnshTalaviya@users.noreply.github.com>
2025-10-03 15:59:30 +0530

    Update ApiHelper.js

--- src/common/service/ApiHelper.js
+++ src/common/service/ApiHelper.js
@@ -1,8 +1,11 @@
 import axios from "axios"
 let Url = "https://kensdrive.co.in/api"
+    console.log(Url)
+
 // let Url = "http://10.223.222.176:8880"
 // let Url = "https://kensdrivebackend-5dp1.onrender.com"
 export const Apihelper = {
+    console.log(Url)
     Register: (data) => {
         return axios.post(Url + "/user/register", data)
     },
